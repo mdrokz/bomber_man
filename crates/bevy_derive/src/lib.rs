fd49d01a0b68a037a71aad64e6c7c9073d31c5e4
mdrokz <mohammadmunshi@gmail.com>
2021-01-21 03:11:20 +0530

    improved derivable macro

--- crates/bevy_derive/src/lib.rs
+++ crates/bevy_derive/src/lib.rs
@@ -6,94 +6,79 @@ extern crate quote;
 use proc_macro::TokenStream;
 
 use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
-use syn::{AttributeArgs};
+use syn::AttributeArgs;
 
 use syn::Lit::Str;
 
 #[proc_macro_attribute]
-pub fn main(input_args: TokenStream, _: TokenStream) -> TokenStream {
+pub fn main(input_args: TokenStream, input_item: TokenStream) -> TokenStream {
     let args = syn::parse_macro_input!(input_args as AttributeArgs);
 
-    build_bevy(args)
+    let item = syn::parse_macro_input!(input_item as syn::ItemFn);
+
+    let body = &item.block.stmts;
+
+    build_bevy(args, body)
 }
 
-fn build_bevy(args: syn::AttributeArgs) -> TokenStream {
-    let mut system_tokens = quote! {};
-    let mut plugin_tokens = quote!{};
+fn build_bevy(args: syn::AttributeArgs, body: &Vec<syn::Stmt>) -> TokenStream {
 
-    for arg in args {
+    let mut fn_tokens:Vec<TokenStream2> = Vec::new();
+
+    for (i, arg) in args.iter().enumerate() {
         match arg {
             syn::NestedMeta::Meta(syn::Meta::NameValue(namevalue)) => {
-                let ident = namevalue.path.get_ident().unwrap();
+                let ident = namevalue.path.get_ident().unwrap().to_string();
 
-                match ident.to_string().as_str() {
-                    "systems" => {
-                        let lit = namevalue.lit.clone();
+                let lit = namevalue.lit.clone();
 
-                        match lit {
-                            Str(v) => {
-                                let v = v.value();
+                match lit {
+                    Str(v) => {
+                        let v = v.value();
 
-                                let systems: Vec<TokenStream2> = v
-                                    .split(",")
-                                    .map(|f| {
-                                        let system_ident = Ident::new(f, Span::call_site());
+                        let tokens: Vec<TokenStream2> = v
+                            .split(",")
+                            .map(|f| {
+                                let ident_token = Ident::new(f, Span::call_site());
 
-                                        quote! {
-                                            #system_ident
-                                        }
-                                    })
-                                    .collect();
+                                quote! {
+                                    #ident_token
+                                }
+                            })
+                            .collect();
 
-                                system_tokens = quote! {
-                                    #(.add_system(#systems.system()))*
-                                };
-                            }
-                            _ => {}
-                        }
-                    }
-                    "plugins" => {
-                        let lit = namevalue.lit.clone();
-
-                        match lit {
-                            Str(v) => {
-                                let v = v.value();
-
-                                let plugins: Vec<TokenStream2> = v
-                                    .split(",")
-                                    .map(|f| {
-                                        let plugin_ident = Ident::new(f, Span::call_site());
-
-                                        quote! {
-                                            #plugin_ident
-                                        }
-                                    })
-                                    .collect();
-
-                                    plugin_tokens = quote! {
-                                    #(.add_plugin(#plugins))*
-                                };
-                            }
-                            _ => {}
+                        if ident.contains("system") {
+                            let mut fn_name = String::from("add_");
+                            fn_name.push_str(&ident);
+                            let ident_fn = Ident::new(&fn_name,Span::call_site());
+                            fn_tokens.push( quote! {
+                                #(.#ident_fn(#tokens.system()))*
+                            });
+                        } else {
+                            let mut fn_name = String::from("add_");
+                            fn_name.push_str(&ident);
+                            let ident_fn = Ident::new(&fn_name,Span::call_site());
+                            fn_tokens.push( quote! {
+                                #(.#ident_fn(#tokens))*
+                            });
                         }
                     }
                     _ => {}
                 }
             }
-            // syn::NestedMeta::Meta(syn::Meta)
-            _ => {
-                // syn::Meta::Path()
-                // println!("{:?}",_)
-            }
+            _ => {}
         }
     }
 
     let t = quote! {
         fn main() {
+            #(#body)*
+
             App::build()
-            // .add_plugins(DefaultPlugins)
-            #system_tokens
-            #plugin_tokens
+            .add_plugins(DefaultPlugins)
+            // #system_tokens
+            // #plugin_tokens
+            #(#fn_tokens)*
             .run();
         }
     };
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{input::system, prelude::*};
 
 #[macro_use]
 extern crate bevy_derive;
@@ -8,18 +8,44 @@ struct Player {
     y: f32
 }
 
-#[bevy_derive::main(systems = "hello_world,test,test1")]
+
+#[bevy_derive::main(system = "hello_world,animate_sprite_system",startup_system = "setup")]
 fn main() {
+
+}
+
+fn animate_sprite_system(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+        timer.tick(time.delta_seconds());
+        if timer.finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+        }
+    }
+}
+
+fn setup(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("resources/sprites/walk_front.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(10.0, 24.0), 7, 2);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands
+        .spawn(Camera2dBundle::default())
+        .spawn(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..Default::default()
+        })
+        .with(Timer::from_seconds(0.1, true));
 }
 
 fn hello_world() {
-    println!("hello world");
-}
-
-fn test() {
-    println!("hello world");
-}
-
-fn test1() {
-    println!("hello world");
+    // println!("hello world");
 }
